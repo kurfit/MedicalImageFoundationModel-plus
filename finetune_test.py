0d02c9fccbd291072deb4c5d906c09acbdfef0c3
kurfit <155168349+kurfit@users.noreply.github.com>
2022-11-14 12:21:01 +0000

    trying travis

--- finetune_test.py
+++ finetune_test.py
@@ -8,4 +8,9 @@ import logging
 import torch.nn as nn
 from monai.networks.nets import ViTAutoEnc
 from monai.data import DataLoader, CacheDataset, CSVSaver
-from monai.transforms i
+from monai.transforms import (
+    LoadImaged,
+    Compose,
+    Resized,
+    ScaleIntensityd
+)
