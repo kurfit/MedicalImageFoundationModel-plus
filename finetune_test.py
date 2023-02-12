b9b25e84a41e3471003aa055a53dc5a8b41237ae
kurfit <155168349+kurfit@users.noreply.github.com>
2023-02-12 10:20:29 +0000

    fix the tests

--- finetune_test.py
+++ finetune_test.py
@@ -43,4 +43,4 @@ def main():
         files_1 = [{"img": img[0], "label": 1} for img in zip(data_1)]
         data = files_0 + files_1
         random.shuffle(data)
-    
+        print('Total Number of training Data Samples:{}'.format(len(da
