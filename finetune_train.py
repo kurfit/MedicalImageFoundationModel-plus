31d84d1809a71f21aaa7b04f0d338f8ffc491361
kurfit <155168349+kurfit@users.noreply.github.com>
2024-02-25 15:48:27 +0000

    Added glossaryitem(s) by lansn-lan

--- finetune_train.py
+++ finetune_train.py
@@ -153,4 +153,4 @@ def main():
                     torch.save(net.state_dict(), "best_metric_classifier.pth")
                     print("saved new best metric model")
                 print(
-          
+                    "current epoch: {} current accuracy: {:.4f} current AUC: {
