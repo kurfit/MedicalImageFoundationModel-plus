fd431edf2df341b7fd571bb66cbaf47d4d7407ce
kurfit <155168349+kurfit@users.noreply.github.com>
2024-03-30 11:50:17 +0000

    0.35.0

--- ssl_pretrain_DDP.py
+++ ssl_pretrain_DDP.py
@@ -122,3 +122,68 @@ def main_worker(args):
     val_ds = CacheDataset(data=test_data,transform=transform)
     val_loader = DataLoader(val_ds, batch_size=batch_size, pin_memory=True, num_workers=2)
     
+    device = torch.device(f"cuda:{args.local_rank}")
+    torch.cuda.set_device(device)
+    
+    model = ViTAutoEnc(
+                in_channels=4,
+                out_channels=4,
+                img_size=(96, 96, 96),
+                patch_size=(16, 16, 16),
+                pos_embed='conv',
+                num_heads=32,
+                num_layers=16,
+                hidden_size=2048,
+                mlp_dim=3072,
+    ).to(device)
+    
+    model = DistributedDataParallel(model, device_ids=[device])
+    
+    recon_loss = L1Loss()
+    contrastive_loss = ContrastiveLoss(temperature=0.05)
+    
+    lr = 1e-4
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+        
+    # Define Hyper-paramters for training loop
+    val_interval = 5
+    
+    
+    epoch_loss_values = []
+    step_loss_values = []
+    epoch_cl_loss_values = []
+    epoch_recon_loss_values = []
+    val_loss_values = []
+    best_val_loss = 1000.0
+        
+    all_start_time = time.time()
+    
+    end=time.time()
+    
+    for epoch in range(args.epochs):   
+        
+        print("-" * 10)
+        print(f"epoch {epoch + 1}/{args.epochs}")
+        model.train()
+        epoch_loss = 0
+        epoch_cl_loss = 0
+        epoch_recon_loss = 0
+        step = 0
+        train_sampler.set_epoch(epoch)
+        end=time.time()
+        for batch_data in train_loader:
+            data_load_time = time.time()-end
+            print(f"Data Load Time: {data_load_time}s")
+            step_start_time = time.time()
+            step += 1
+            optimizer.zero_grad()
+            #start_time = time.time()
+    
+            inputs, inputs_2, gt_input = (
+                batch_data["img_drop_1"].to(device),
+                batch_data["img_drop_2"].to(device),
+                batch_data["gt_img"].to(device),
+            )
+            
+            outputs_v1, hidden_v1 = model(inputs)
+            outputs_v2, hidden_v2 = model(inputs_2)
